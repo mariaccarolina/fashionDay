68bad2d8e69ae13e4a9cc2b97a8bb9a17f3ca2a9
Maria Carolina <demycar24@gmail.com>
2025-07-14 21:44:51 -0300

    :zap: perf: Melhoria no código

--- src/components/Linktree.jsx
+++ src/components/Linktree.jsx
@@ -9,33 +9,53 @@ export default function Linktree() {
         <nav>
           <ul>
             <li>
-              <a href="https://wa.me/5581987901785" target="_blank">
+              <a
+                className="lojas"
+                href="https://wa.me/5581987901785"
+                target="_blank"
+              >
                 <img className="whats" src="./whats.png" alt="Whats" />
                 <p>Loja Ribeirão</p>
               </a>
             </li>
             <li>
-              <a href="https://wa.me/5581987918380" target="_blank">
+              <a
+                className="lojas"
+                href="https://wa.me/5581987918380"
+                target="_blank"
+              >
                 <img className="whats" src="./whats.png" alt="Whats" />
                 <p>Loja Escada</p>
               </a>
             </li>
             <li>
-              <a href="https://wa.me/5581985348454" target="_blank">
+              <a
+                className="lojas"
+                href="https://wa.me/5581985348454"
+                target="_blank"
+              >
                 <img className="whats" src="./whats.png" alt="Whats" />
                 <p>Loja Palmares</p>
               </a>
             </li>
             <li>
-              <a href="https://wa.me/5581986950572" target="_blank">
+              <a
+                className="lojas"
+                href="https://wa.me/5581986950572"
+                target="_blank"
+              >
                 <img className="whats" src="./whats.png" alt="Whats" />
                 <p>Loja Cabo de Santo Agostinho</p>
               </a>
             </li>
           </ul>
         </nav>
-        <div className="container2">
-          <a href="https://www.instagram.com/fashion.dayi/" target="_blank">
+        <div className="containerInsta">
+          <a
+            className="container2"
+            href="https://www.instagram.com/fashion.dayi/"
+            target="_blank"
+          >
             <img className="insta" src="./insta.png" alt="instagram" />
             <p>@fashion.day</p>
           </a>
