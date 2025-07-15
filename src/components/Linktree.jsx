ccfa657a95f547e0efa96c73010510b1f8c14521
Maria Carolina <demycar24@gmail.com>
2025-07-14 21:19:48 -0300

    :sparkles: feat: Ajustes

--- src/components/Linktree.jsx
+++ src/components/Linktree.jsx
@@ -4,30 +4,30 @@ export default function Linktree() {
   return (
     <main className="container">
       <section>
-        <img className="logo" src="./day.png" alt="" />
+        <img className="logo" src="./day.png" alt="logomarca" />
         <p className="bordao">A loja mais tombadora da cidade!</p>
         <nav>
           <ul>
             <li>
-              <a href="https://wa.me/5581987901785">
+              <a href="https://wa.me/5581987901785" target="_blank">
                 <img className="whats" src="./whats.png" alt="Whats" />
                 <p>Loja Ribeirão</p>
               </a>
             </li>
             <li>
-              <a href="https://wa.me/5581987918380">
+              <a href="https://wa.me/5581987918380" target="_blank">
                 <img className="whats" src="./whats.png" alt="Whats" />
                 <p>Loja Escada</p>
               </a>
             </li>
             <li>
-              <a href="https://wa.me/5581985348454">
+              <a href="https://wa.me/5581985348454" target="_blank">
                 <img className="whats" src="./whats.png" alt="Whats" />
                 <p>Loja Palmares</p>
               </a>
             </li>
             <li>
-              <a href="https://wa.me/5581986950572">
+              <a href="https://wa.me/5581986950572" target="_blank">
                 <img className="whats" src="./whats.png" alt="Whats" />
                 <p>Loja Cabo de Santo Agostinho</p>
               </a>
@@ -35,8 +35,10 @@ export default function Linktree() {
           </ul>
         </nav>
         <div className="container2">
-          <img className="insta" src="./insta.png" alt="" />
-          <p>@fashion.day</p>
+          <a href="https://www.instagram.com/fashion.dayi/" target="_blank">
+            <img className="insta" src="./insta.png" alt="instagram" />
+            <p>@fashion.day</p>
+          </a>
         </div>
       </section>
     </main>
