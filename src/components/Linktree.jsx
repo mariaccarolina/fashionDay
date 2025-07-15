8519b8b2c375f90c32733aa3182c7227f8b93bc2
Maria Carolina <demycar24@gmail.com>
2025-07-14 21:12:17 -0300

    :sparkles: feat: Atualizações

--- src/components/Linktree.jsx
+++ src/components/Linktree.jsx
@@ -9,28 +9,29 @@ export default function Linktree() {
         <nav>
           <ul>
             <li>
-              <a href="">
-                <img className="whats" src="./whats.png" alt="" />
+              <a href="https://wa.me/5581987901785">
+                <img className="whats" src="./whats.png" alt="Whats" />
                 <p>Loja Ribeirão</p>
-              </a></li>
-              <li>
-                <a href="">
-                <img className="whats" src="./whats.png" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="https://wa.me/5581987918380">
+                <img className="whats" src="./whats.png" alt="Whats" />
                 <p>Loja Escada</p>
               </a>
-              </li>
-              <li>
-                <a href="">
-                <img className="whats" src="./whats.png" alt="" />
+            </li>
+            <li>
+              <a href="https://wa.me/5581985348454">
+                <img className="whats" src="./whats.png" alt="Whats" />
                 <p>Loja Palmares</p>
               </a>
-              </li>
-              <li>
-                 <a href="">
-                <img className="whats" src="./whats.png" alt="" />
+            </li>
+            <li>
+              <a href="https://wa.me/5581986950572">
+                <img className="whats" src="./whats.png" alt="Whats" />
                 <p>Loja Cabo de Santo Agostinho</p>
               </a>
-              </li>
+            </li>
           </ul>
         </nav>
         <div className="container2">
